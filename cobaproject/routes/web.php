8a94bedf63f1fe1080b85f49fa9f65f7130c11cc
rezazafirah66@gmail.com <rezazafirah66@gmail.com>
2025-02-08 14:02:15 +0800

    update baris 10 reza

--- cobaproject/routes/web.php
+++ cobaproject/routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello World";
 });
 
 Route::get('/test', function () {
-    return "Hello World";
+    return "halo ekal";
 });
